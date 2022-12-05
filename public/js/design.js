d869d0edb4fd9315e1d86d9b12eaaad526f254c2
Jonx <dev.jonx.eu@gmail.com>
2022-12-05 07:07:11 +0100

    added Msg Send Bar

--- public/js/design.js
+++ public/js/design.js
@@ -1,1 +1,15 @@
-alert();
+auto_grow(document.getElementById("tempId3232"));
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+
+    //document.getElementById("chatEndBox").style.backgroundColor = "#333"
+
+    if( element.style.height >= "50px"){
+        document.getElementById("chatEndBox").style.height = element.style.height;
+    } else {
+        document.getElementById("chatEndBox").style.height = "65px"
+    }
+    document.getElementById("chatEndBox").scrollIntoView();
+}
